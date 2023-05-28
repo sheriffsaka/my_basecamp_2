09cee39858c4694252f97cb4828414b5df80ec01
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2023-05-28 19:40:27 +0100

    Added user controller and index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   def set_default_role
     self.role ||= :user
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :projects
+  resources :users
   #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
