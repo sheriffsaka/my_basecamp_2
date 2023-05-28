8f8239d0a93fd9f270410aa23e34200e5fe925f6
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2023-05-28 19:06:01 +0100

    Added new column role to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    include Pundit::Authorization
+
+    rescue_from Pundit::NotAuthorizedError, with: :pundishing_user
+
+    private
+
+  def pundishing_user
+    flash[:notice] = "You are not authorized to perform this action."
+    redirect_to root_path
+  end
 end
---
+++ db/migrate/20230528173111_remove_admin_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveAdminFromUser < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :admin, :boolean
+  end
+end
